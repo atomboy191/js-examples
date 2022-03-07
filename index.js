e359a1c167d56097f22ae97101d9225ff6438fdc
Nick Tabing <nicknight9@gmail.com>
2022-03-07 13:18:18 -0800

    node callback and error handling

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var rect = require('./rectangle');
+var tri = 0;
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b =" + b);
-    if(l<0 || b<0){
-        console.log("Dimension should be positv");
-        return;
-    }
 
-    console.log("area is " + rect.perimeter(l,b) + " and area is " + rect.area(l,b)); 
+    rect(l,b, function(err, rectangle) {
+        if(err){
+            console.log('ERROR: ', err.message);
+        } else {
+            console.log("Perimeter is: ", rectangle.perimeter(l,b));
+            console.log("Area is: ", rectangle.area(l,b));
+        }
+     });
+     console.log("this statement is after the call to rect");
 }
 
 solveRect(10,20);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,21 @@
-exports.perimeter = (x,y) => (x+y)*2;
-exports.area = (x,y) => x*y;
+module.exports = (x,y,callback) => {
+    if( x <= 0 || y <= 0){
+        setTimeout(() => 
+            callback(new Error("Rectangle's dimension should be positve " + x + ' ' + y), null),
+            2000);
+    } else {
+        setTimeout(() =>
+            callback(null, 
+                {
+                    perimeter: () => (x+y)*2,
+                    area: () => x*y
+                })
+            , 5000);
+
+    }
+
+}
+
+//console.log("area is " + rect.perimeter(l,b) + " and area is " + rect.area(l,b)); 
+
+
